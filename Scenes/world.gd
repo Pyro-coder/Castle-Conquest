fb339c68038766d72197a252d70fb816dbb165d3
Noah Oppermann <oppermann.noah@gmail.com>
2025-03-03 13:11:44 -0600

    Finalized rotating pixel background

--- Scenes/world.gd
+++
@@ -1,9 +0,0 @@
-extends Node3D
-
-@onready var camera_pivot = $CameraPivot
-
-var rotation_speed = 8
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	camera_pivot.rotation_degrees.y += delta * rotation_speed

98600013d769b8503f72ccb2c2955ac015daa927
Noah Oppermann <oppermann.noah@gmail.com>
2025-03-11 20:43:28 -0500

    fixed camera

--- Scenes/camera_3d.gd
+++ Scenes/camera_3d.gd
@@ -1,143 +1,70 @@
-class_name FreeLookCamera extends Camera3D
+extends Camera3D
+class_name FixedAngleCamera
 
-# This was free code provided for free online by Adam Viola
+@export var move_speed: float = 10.0
 
-# Modifier keys' speed multiplier
-const SHIFT_MULTIPLIER = 2.5
-const ALT_MULTIPLIER = 1.0 / SHIFT_MULTIPLIER
+var dynamic_min_x: float
+var dynamic_max_x: float
+var dynamic_min_z: float
+var dynamic_max_z: float
 
+func _ready() -> void:
+	# Keep the camera angle/position you want:
+	rotation_degrees = Vector3(-70.0, 90.0, 0.0)
+	position = Vector3(-40.0, 20.0, -50.0)
 
-@export_range(0.0, 1.0) var sensitivity: float = 0.25
+func _physics_process(delta: float) -> void:
+	_update_dynamic_bounds()
 
-# Mouse state
-var _mouse_position = Vector2(0.0, 0.0)
-var _total_pitch = 0.0
+	# 1) Gather input for WASD / Arrow keys (The directions are also tilted 90 degrees)
+	var input_dir = Vector2.ZERO
+	if Input.is_action_pressed("ui_up"):    # W or Up Arrow
+		input_dir.x -= 3
+	if Input.is_action_pressed("ui_down"):  # S or Down Arrow
+		input_dir.x += 3
+	if Input.is_action_pressed("ui_left"):  # A or Left Arrow
+		input_dir.y += 3
+	if Input.is_action_pressed("ui_right"): # D or Right Arrow
+		input_dir.y -= 3
 
-# Movement state
-var _direction = Vector3(0.0, 0.0, 0.0)
-var _velocity = Vector3(0.0, 0.0, 0.0)
-var _acceleration = 30
-var _deceleration = -10
-var _vel_multiplier = 4
+	# 2) Move strictly in global XZ, ignoring camera rotation
+	if input_dir != Vector2.ZERO:
+		input_dir = input_dir.normalized()
+		# Here, "input_dir.y" moves along global Z, and "input_dir.x" along global X.
+		# Negative Z is "forward" if you prefer W to go 'forward' on the plane.
+		var move_vec = Vector3(input_dir.x, 0.0, input_dir.y) * move_speed * delta
+		position += move_vec
 
-# Keyboard state
-var _w = false
-var _s = false
-var _a = false
-var _d = false
-var _q = false
-var _e = false
-var _shift = false
-var _alt = false
+		# 3) Clamp the camera so it cannot pan away from your pieces
+		position.x = clamp(position.x, dynamic_min_x, dynamic_max_x)
+		position.z = clamp(position.z, dynamic_min_z, dynamic_max_z)
 
-#Test functionality
-#var board_center = Vector3.ZERO
-#var smoothing_factor = 0.1
+func _update_dynamic_bounds() -> void:
+	# Dynamically compute bounding box around all "Pieces" in the scene
+	var piece_nodes = get_tree().get_nodes_in_group("Pieces")
+	if piece_nodes.is_empty():
+		# If no pieces exist, clamp to some default
+		dynamic_min_x = -100
+		dynamic_max_x =  100
+		dynamic_min_z = -100
+		dynamic_max_z =  100
+		return
 
-#var fov_zoom_level = 60.0
-#@export_range(1, 120) var max_fov = 100
-#@export_range(1, 120) var min_fov = 30
-#
-#var camera_height = 10.0
-#end of test
+	var min_px = INF
+	var max_px = -INF
+	var min_pz = INF
+	var max_pz = -INF
 
-func _input(event):
-	# Receives mouse motion
-	if event is InputEventMouseMotion:
-		_mouse_position = event.relative
-	
-	# Receives mouse button input
-	if event is InputEventMouseButton:
-		match event.button_index:
-			MOUSE_BUTTON_RIGHT: # Only allows rotation if right click down
-				Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED if event.pressed else Input.MOUSE_MODE_VISIBLE)
-			MOUSE_BUTTON_WHEEL_UP: # Increases max velocity
-				_vel_multiplier = clamp(_vel_multiplier * 1.1, 0.2, 20)
-			MOUSE_BUTTON_WHEEL_DOWN: # Decereases max velocity
-				_vel_multiplier = clamp(_vel_multiplier / 1.1, 0.2, 20)
+	for piece in piece_nodes:
+		var piece_pos = piece.global_transform.origin
+		min_px = min(min_px, piece_pos.x)
+		max_px = max(max_px, piece_pos.x)
+		min_pz = min(min_pz, piece_pos.z)
+		max_pz = max(max_pz, piece_pos.z)
 
-	# Receives key input
-	if event is InputEventKey:
-		match event.keycode:
-			KEY_W:
-				_w = event.pressed
-			KEY_S:
-				_s = event.pressed
-			KEY_A:
-				_a = event.pressed
-			KEY_D:
-				_d = event.pressed
-			KEY_Q:
-				_q = event.pressed
-			KEY_E:
-				_e = event.pressed
-			KEY_SHIFT:
-				_shift = event.pressed
-			KEY_ALT:
-				_alt = event.pressed
-
-# Updates mouselook and movement every frame
-func _process(delta):
-	_update_mouselook()
-	_update_movement(delta)
-	#Test function
-	#_center_camera_on_board(delta)
-
-# Updates camera movement
-func _update_movement(delta):
-	# Computes desired direction from key states
-	_direction = Vector3(
-		(_d as float) - (_a as float), 
-		(_e as float) - (_q as float),
-		(_s as float) - (_w as float)
-	)
-	
-	# Computes the change in velocity due to desired direction and "drag"
-	# The "drag" is a constant acceleration on the camera to bring it's velocity to 0
-	var offset = _direction.normalized() * _acceleration * _vel_multiplier * delta \
-		+ _velocity.normalized() * _deceleration * _vel_multiplier * delta
-	
-	# Compute modifiers' speed multiplier
-	var speed_multi = 1
-	if _shift: speed_multi *= SHIFT_MULTIPLIER
-	if _alt: speed_multi *= ALT_MULTIPLIER
-	
-	# Checks if we should bother translating the camera
-	if _direction == Vector3.ZERO and offset.length_squared() > _velocity.length_squared():
-		# Sets the velocity to 0 to prevent jittering due to imperfect deceleration
-		_velocity = Vector3.ZERO
-	else:
-		# Clamps speed to stay within maximum value (_vel_multiplier)
-		_velocity.x = clamp(_velocity.x + offset.x, -_vel_multiplier, _vel_multiplier)
-		_velocity.y = clamp(_velocity.y + offset.y, -_vel_multiplier, _vel_multiplier)
-		_velocity.z = clamp(_velocity.z + offset.z, -_vel_multiplier, _vel_multiplier)
-	
-		translate(_velocity * delta * speed_multi)
-
-# Updates mouse look 
-func _update_mouselook():
-	# Only rotates mouse if the mouse is captured
-	if Input.get_mouse_mode() == Input.MOUSE_MODE_CAPTURED:
-		_mouse_position *= sensitivity
-		var yaw = _mouse_position.x
-		var pitch = _mouse_position.y
-		_mouse_position = Vector2(0, 0)
-		
-		# Prevents looking up/down too far
-		pitch = clamp(pitch, -90 - _total_pitch, 90 - _total_pitch)
-		_total_pitch += pitch
-	
-		rotate_y(deg_to_rad(-yaw))
-		rotate_object_local(Vector3(1,0,0), deg_to_rad(-pitch))
-		
-# Smoothly centers the camera on the board's ceneter
-#func _center_camera_on_board(delta):
-#	position = position.lerp(board_center + Vector3(0, camera_height, 0), smoothing_factor * delta)
-#	look_at(board_center, Vector3(0,1,0))
-	
-#	var fov_factor = max(board_center.distance_to(position) / 10.0, min_fov)
-#	fov = clamp(fov_factor, min_fov, max_fov)
-	
-	
-#func update_board_center(new_center: Vector3):
-#	board_center = new_center
+	# Add some margin so the camera doesn't sit right on the edge
+	var margin = 10.0
+	dynamic_min_x = min_px - margin + 15
+	dynamic_max_x = max_px + margin - 3
+	dynamic_min_z = min_pz - margin + 5
+	dynamic_max_z = max_pz + margin - 5

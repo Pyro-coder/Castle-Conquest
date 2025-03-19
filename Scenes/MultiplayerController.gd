73299eee842c3403301ac7be9e166bdd4ed783b7
Ivy Henderson <ihenderson@harding.edu>
2025-03-19 11:51:49 -0500

    Initial push

---
+++ Scenes/MultiplayerController.gd
@@ -0,0 +1,75 @@
+extends Control
+
+@export var Address = '127.0.0.1'
+@export var port = 1234
+
+var peer
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	multiplayer.peer_connected.connect(peer_connected)
+	multiplayer.peer_disconnected.connect(peer_disconnected)
+	multiplayer.connected_to_server.connect(connected_to_server)
+	multiplayer.connection_failed.connect(connection_failed)
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+func peer_connected(id):
+	print("Player Connected" + str(id))
+
+func peer_disconnected(id):
+	print("Player Disconnected" + str(id))
+
+func connected_to_server():
+	print("Connected to server!")
+
+func connection_failed():
+	print("Couldn't connect")
+
+@rpc("any_peer", "call_local")
+func StartGame():
+	var scene = load("res://Scenes/board.tscn") #figure out what to put in here to play the game
+	
+	if scene == null:
+		print("Failed to load the scene.")
+		return
+	else:
+		print("Scene successfully loaded!")
+	
+	var instance = scene.instantiate()
+	
+	if instance == null:
+		print("Failed to instantiate the scene.")
+		return
+	else:
+		print("Scene successfully instantiated")
+		
+	get_tree().root.add_child(instance)
+	self.hide()
+
+func _on_host_button_down() -> void:
+	peer = ENetMultiplayerPeer.new()
+	var error = peer.create_server(port, 2)
+	if error != OK:
+		print("cannot host: " + error)
+		return
+	
+	multiplayer.set_multiplayer_peer(peer)
+	print("Waiting for players!")
+	pass # Replace with function body.
+
+
+func _on_join_button_down() -> void:
+	peer = ENetMultiplayerPeer.new()
+	peer.create_client(Address, port)
+	multiplayer.set_multiplayer_peer(peer)
+	pass # Replace with function body.
+
+
+func _on_start_game_button_down() -> void:
+	StartGame.rpc()
+	pass # Replace with function body.

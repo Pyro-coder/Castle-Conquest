6e3799cba82b351fd9a2e39f6671f84c19d92168
dbeneby <dbeneby1@harding.edu>
2025-04-05 18:21:07 -0500

    quit button

--- Scenes/Menus/game_over_screen.gd
+++ Scenes/Menus/game_over_screen.gd
@@ -1,12 +1,15 @@
 extends CanvasLayer
 
 @onready var title = $PanelContainer/MarginContainer/VBoxContainer/Title
+@onready var restartBtn = $PanelContainer/MarginContainer/VBoxContainer/CenterContainer/HBoxContainer/RestartBtn
+@onready var menuBtn = $PanelContainer/MarginContainer/VBoxContainer/CenterContainer/HBoxContainer/MenuBtn
+@onready var quitBtn = $PanelContainer/MarginContainer/VBoxContainer/CenterContainer/HBoxContainer2/QuitBtn
 @onready var audio_player: AudioStreamPlayer = $AudioStreamPlayer
 
 func _ready() -> void:
 	AudioPlayer.stop()
 	audio_player.play()
-	
+
 
 func set_title(win: bool):
 	if win:
@@ -19,5 +22,9 @@ func _on_restart_btn_pressed() -> void:
 	get_tree().change_scene_to_file("res://Scenes/Menus/coinflipscreen.tscn")
 
 
-func _on_quit_btn_pressed() -> void:
+func _on_menu_btn_pressed() -> void:
 	get_tree().change_scene_to_file("res://Scenes/Menus/main_menu.tscn")
+	
+func _on_quit_btn_pressed() -> void:
+	get_tree().quit()
+	
